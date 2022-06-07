362de29e6fff422e16f7ef6f298ad971680bb296
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-06-07 12:14:28 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// const root = ReactDOM.createRoot();
-// console.log(store.getState());
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(store.getState());
+root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
   // </React.StrictMode>
 );
 
