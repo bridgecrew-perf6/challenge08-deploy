11ca6517ed161224b7f9b20d53c272f2796682ba
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-06-09 10:37:50 +0700

    update backend

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'https://challenge08-binar.herokuapp.com/api/auth/';
 
 /**
  * 6. Ini merupakan function untuk melakukan HTTP Request POST untuk menambahkan username, email, dan password
